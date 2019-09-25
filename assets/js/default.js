e1048350c4a809f99b918f8fadbba9c5512880c9
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-09-26 00:41:50 +0700

    Add recently used list

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,9 +1,54 @@
 const timer = Timer();
+const MAX_RECENT = 10;
+
+function identicalArray(a, b) {
+  return JSON.stringify(a) === JSON.stringify(b);
+};
+
+function clearRecentlyUsed() {
+  localStorage.removeItem('timers');
+  refreshRecentlyUsed();
+}
+
+function addToRecentlyUsed(t) {
+  let seq = JSON.parse(localStorage.getItem('timers') || '[]');
+  if ((seq.length === 0) || ((seq.length > 0) && (!identicalArray(seq[0], t)))) {
+    seq.unshift(t);
+    if (seq.length > MAX_RECENT) {
+      seq.pop();
+    }
+    localStorage.setItem('timers', JSON.stringify(seq));
+    refreshRecentlyUsed();
+  }
+}
+
+function refreshRecentlyUsed() {
+  let seq = JSON.parse(localStorage.getItem('timers') || '[]');
+
+  $('#recentDropdownItems [role="setTimer"]').remove();
+  $('#noRecent').hide();
+  if (seq.length > 0) {
+    $('#recentDropdownItems').prepend(seq.map(function (t) {
+      const dataset = t.toString();
+      const friendlyDataSet = toFriendlyTimeSeq(t);
+      return $(`<a class="dropdown-item" href="#" role="setTimer" aria-dataset="${dataset}">${friendlyDataSet}</a>`);
+    }));
+    $('#recentDropdownItemsDivider,#clearRecent').show();
+  } else {
+    $('#recentDropdownItemsDivider,#clearRecent').hide();
+    $('#noRecent').show();
+  }
+}
 
 function setTimerSequence() {
   const dataSet = $(this).attr('aria-dataset').split(',');
   const timeSeq = dataSet.map(n => parseInt(n, 10));
+  setTimer(timeSeq);
+}
+
+function setTimer(timeSeq) {
   timer.set(timeSeq);
+  addToRecentlyUsed(timeSeq);
 }
 
 function startTimer() {
@@ -22,6 +67,31 @@ window.onunload = () => {
   timer.stop();
 }
 
+function toFriendlyTimeSeq(t) {
+  return t.map(toReadableTime).join(' > ');
+}
+
+function toReadableTime(t) {
+  let s = '';
+  let tm = 0;
+  if (t % 3600 === 0) {
+    s = (t === 3600) ? '' : 's';
+    tm = (t / 3600);
+    return `${tm} hr${s}`;
+  }
+  if (t % 60 === 0) {
+    s = (t === 60) ? '' : 's';
+    tm = (t / 60);
+    return `${tm} min${s}`;
+  }
+  s = (t === 1) ? '' : 's';
+  return `${t} sec${s}`;
+}
+
+Array.prototype.friendly = () => {
+  return toFriendlyTimeSeq(this);
+};
+
 function toFriendlyTime(n) {
   const m = n % 60;
   const h = (n - m) / 60;
@@ -32,7 +102,7 @@ function toFriendlyTime(n) {
 
 Number.prototype.friendly = () => {
   return toFriendlyTime(this);
-}
+};
 
 function drawSlot(count, active) {
   $('#time-slots').empty();
@@ -46,7 +116,7 @@ function applyTimeSeries() {
   const customTimeSeries = $('#customTimeSeries').val();
   if (tryParseTimeSeries(customTimeSeries)) {
     const timeSeq = parseTimeSeries(customTimeSeries);
-    timer.set(timeSeq);
+    setTimer(timeSeq);
     $('#customTimeSeries').removeClass('is-invalid');
     $('#customTimerModal').modal('hide');
   } else {
@@ -143,9 +213,12 @@ $(() => {
     }
   });
 
-  $('[role="setTimer"]').click(setTimerSequence);
+  $('body').on('click', '[role="setTimer"]', setTimerSequence);
+  $('body').on('click', '#clearRecent', clearRecentlyUsed);
   $('[role="startTimer"]').click(startTimer);
   $('[role="pauseTimer"]').click(pauseTimer);
   $('[role="stopTimer"]').click(stopTimer);
   $('#applyCustomTimeSeries').click(applyTimeSeries);
+
+  refreshRecentlyUsed();
 });
